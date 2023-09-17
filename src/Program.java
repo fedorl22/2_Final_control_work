5d8322d29938a53e0078937cec37d505a3e47eb5
Fedor <fdr@mail.ru>
2023-09-17 21:21:00 +0300

    add prog

---
+++ src/Program.java
@@ -0,0 +1,17 @@
+import Controller.PetController;
+import Model.Cat;
+import Model.Dog;
+import Model.Pet;
+import Services.IRepository;
+import Services.PetRepository;
+import UserInterface.ConsoleMenu;
+
+public class Program {
+    public static void main(String[] args) throws Exception {
+
+        IRepository <Pet> myFarm = new PetRepository();
+        PetController controller = new PetController(myFarm);
+        new ConsoleMenu(controller).start();
+    
+    }
+}    
